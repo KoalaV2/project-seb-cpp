cbd4212e8c709b81459c51520312195a21050fd0
KoalaV2 <theo.johanson@gmail.com>
2019-05-25 15:37:19 +0200

    Changed order on some things in the calculator

--- seb.cpp
+++ seb.cpp
@@ -14,10 +14,10 @@ int main() {
             char calc;
             std::cout << "Please enter your first number! \n :";
             std::cin >> first_number;
-            std::cout << "Please enter your second number! \n :";
-            std::cin >> second_number;
-            std::cout << "Please enter how you want to calculate it! \n :";
+	    std::cout << "Please enter how you want to calculate it! \n :";
             std::cin >> calc;
+	    std::cout << "Please enter your second number! \n :";
+            std::cin >> second_number;
             std::cout << "\n";
             switch(calc) {
             case '+':
