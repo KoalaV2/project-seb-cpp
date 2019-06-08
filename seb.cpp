62191c17474c116fbe5bbd8f3fedd432209cbdf3
KoalaV2 <theo.johanson@gmail.com>
2019-06-08 15:24:36 +0200

    Added some more tts stuff and used getline so i can now have use names with spaces for example (open calculator)

--- seb.cpp
+++ seb.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <iostream>
+#include <string>
 #include <festival/festival.h>
 int main() {
     EST_Wave wave;
@@ -11,13 +12,17 @@ int main() {
     float result;
 
     std::cout << "Hello user! What's your name? \n :";
-    festival_say_text("Hello user! What's your name?");
+    festival_say_text("Hello user! What's your name? \n :");
     std::cin >> name;
-    std::cout << "Welcome " << name << "! What do you want to do today? \n :";
-    festival_say_text("What do you want to do today?");
-    std::cin >> use;
+    std::cout << "Welcome " << name;
     if ( name == "Theo" ) {
-        if ( use == "open_calculator") {
+        std::cout << ", What do you want to do today? \n :";
+        name.append(", What do you want to do today?");
+        festival_say_text(name.c_str());
+        std::cin.ignore();
+        std::getline(std::cin, use);
+        // std::cin >> use;
+        if ( use == "open calculator") {
             float first_number;
             float second_number;
             char calc;
@@ -51,10 +56,10 @@ int main() {
             std::cout << '=' << result << "\n";
 
         }
-        else if ( use == "ssh_info" ) {
+        else if ( use == "ssh info" ) {
             system("/bin/bash -c ./ssh.sh");
         }
-        else if ( use == "time_info" ) {
+        else if ( use == "time info" ) {
             time_t tt;
             struct tm * ti;
             time(&tt);
@@ -62,20 +67,29 @@ int main() {
             std::cout << "The current time is :" << asctime(ti);
         }
         else if ( use == "tts" ) {
-            festival_say_text("Hello world!"); // Eventually adding user input.
+            std::string stuff;
+            festival_say_text("What do you want me to say?");
+            std::cout << "What do you want me to say?" << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin,stuff);
+            std::cout << stuff;
+            festival_say_text(stuff.c_str());
         }
         else if ( use == "update") {
-            festival_say_text("Updating the system now!");
+              festival_say_text("Updating the system now!");
             system("yay -Syu --sudoloop");
         }
         else {
+            festival_say_text("Error, the command which is inputted has not been added yet!");
             std::cout << "ERROR, the command which is inputted has not been added yet.";
         }
     }
     else if ( name != "Theo") {
+        festival_say_text("Access denied, user is not authorized");
         std::cout << "Access denied. User is not authorized!";
     }
     else {
+        festival_say_text("ERROR, something went wrong!");
         std::cout << "ERROR, something went wrong!";
     }
 }
